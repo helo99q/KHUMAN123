e4bcf183a3125540480c958607d9d2098fef80e2
helo99q <lenillaishram750@gmail.com>
2025-03-13 11:54:23 +0530

    sign

---
+++ sign/signn.java
@@ -0,0 +1,11 @@
+public class signn {
+    public static void main(String args[]){
+        int x = 20;
+        if(x<=0){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+}
