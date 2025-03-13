58c98c45ef8b6febd53b8df7fc8dc67f41fdc518
helo99q <lenillaishram750@gmail.com>
2025-03-13 12:20:02 +0530

    array

---
+++ Array/simple.java
@@ -0,0 +1,8 @@
+public class simple {
+    public static void main(String args[]){
+        int a[] ={12,23,34,45,56,67,78,89};
+        System.out.println(a[1]);
+        System.out.println(a[5]);
+        System.out.println(a[7]);
+    }
+}
