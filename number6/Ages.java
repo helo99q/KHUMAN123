3f914f6699247ad89298b73868bae2547a35ff6e
helo99q <lenillaishram750@gmail.com>
2025-03-13 15:23:40 +0530

    ages

---
+++ number6/Ages.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Ages{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your age");
+        int age = input.nextInt();
+        System.out.println("my age is " +age);
+    }
+}
