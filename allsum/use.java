e869d437a13d909fea768c7d350a5e832fd0a1a4
helo99q <lenillaishram750@gmail.com>
2025-03-13 10:44:53 +0530

    simple con

---
+++ allsum/use.java
@@ -0,0 +1,10 @@
+public class use {
+    public static void main(String args[]){
+        System.out.println(11+22);
+        System.out.println(33-11);
+        System.out.println(55*3);
+        System.out.println(33/6);
+        System.out.println(100%10);
+    }
+
+}
---
+++ another/condition.java
@@ -0,0 +1,11 @@
+public class condition {
+    public static void main(String args[]){
+        System.out.println(11<22);
+        System.out.println(33>11);
+        System.out.println(55<=73);
+        System.out.println(33==6);
+        System.out.println(100!=10);
+    }
+
+}
+ 
