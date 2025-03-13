109842e35e06197378bd25990ea086b0f1cf9096
helo99q <lenillaishram750@gmail.com>
2025-03-13 14:21:48 +0530

    even

---
+++ number2/evenn.java
@@ -0,0 +1,9 @@
+public class evenn {
+    public static void main(String args[]){
+        for(int i=4; i<9; i++){
+            if(i%4==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
