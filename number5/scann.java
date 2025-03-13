48037458758c7c8d3772f358265d24a140b3ca94
helo99q <lenillaishram750@gmail.com>
2025-03-13 15:11:19 +0530

    scanner

---
+++ number5/scann.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class scann{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is your name");
+        String name = input.nextLine();
+        System.out.println("bob");
+    }
+}
