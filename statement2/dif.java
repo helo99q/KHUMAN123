328af844e7a5a238d2ed16254e7007d35831a7a0
helo99q <lenillaishram750@gmail.com>
2025-03-13 11:47:49 +0530

    major minor

---
+++ statement2/dif.java
@@ -0,0 +1,11 @@
+public class dif {
+    public static void main(String args[]){
+        int x = 20;
+        if(x<=50){
+            System.out.println("minor");
+        }
+        else{
+            System.out.println("major");
+        }
+    }
+}
