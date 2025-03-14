c2401065e2cb7b7f0b660ab48f5b1bae47549dc3
helo99q <lenillaishram750@gmail.com>
2025-03-14 10:29:54 +0530

    add_number

---
+++ add_number/addd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class addd {
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter number");
+        int a = obj.nextInt();
+        int i=1;
+        while(i <=a){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
