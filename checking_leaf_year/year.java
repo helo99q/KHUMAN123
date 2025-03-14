0c62f649c2c0d470c198917f252c4395089ab339
helo99q <lenillaishram750@gmail.com>
2025-03-14 08:21:33 +0530

    simple program

--- checking_leaf_year/year.java
+++ checking_leaf_year/year.java
@@ -1,7 +1,7 @@
 public class year {
     public static void main(String args[]){
         int i = 2010 ;
-        if(i%5==0){
+        if(i%4==0){
             System.out.println(i + " leaf year");
         }
         else{
---
+++ gender_finder/simple.java
@@ -0,0 +1,13 @@
+package gender_finder;
+
+public class simple {
+    public static void main(String[] args) {
+        String a = "M";
+        if(a =="M"){
+            System.err.println("Male");
+        }
+        else{
+            System.out.println("Female");
+        }
+    }
+}
