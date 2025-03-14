c1d745d70c114eb5007e34a7125e93bf17ce5b99
helo99q <lenillaishram750@gmail.com>
2025-03-14 08:36:58 +0530

    simple code

---
+++ dynamic_addition/simple.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class simple {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.println("enter fist number");
+        int a = name.nextInt();
+        System.out.println("enter second number");
+        int b = name.nextInt();
+        int c = a + b;
+       System.out.println("the sum  is " +c);
+        
+    }
+}
---
+++ even_number/simiple.java
@@ -0,0 +1,11 @@
+package even_number;
+
+public class simiple {
+    public static void main(String[] args) {
+        for(int i =11;i<=20;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
