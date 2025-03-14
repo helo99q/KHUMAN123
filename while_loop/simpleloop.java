b9c6f9e00a68c84438dcef104fa5ce0332cf02bf
helo99q <lenillaishram750@gmail.com>
2025-03-14 10:14:50 +0530

    while_loop

---
+++ while_loop/simpleloop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class simpleloop {
+    public static void main(String args[]){
+        int i= 34;
+        while(i < 40){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
