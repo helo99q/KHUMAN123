03580126b70bbc0022dee9cee801d696c3e6ea78
helo99q <lenillaishram750@gmail.com>
2025-03-14 10:49:24 +0530

    seven_multiple

---
+++ 7multiple/Mtable.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Mtable{
+    public static void main(String args[]){
+        int number = 7;
+        for(int i = 1; i <= 10;i++){
+            System.out.println(number + "*" + i +"=" +(number * i));
+        }
+
+    }
+}
