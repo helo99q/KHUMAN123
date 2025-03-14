f78820aa656b146bc58e14580c0234ce06e15006
helo99q <lenillaishram750@gmail.com>
2025-03-14 11:54:26 +0530

    splitting

---
+++ spliting/split.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class split {
+    public static void main(String args[]){
+        String name = "anaconda,python";
+        String names[] = name.split(",");
+        System.out.println(names[0]);
+    } 
+}
