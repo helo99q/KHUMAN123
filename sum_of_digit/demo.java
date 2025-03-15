1d728e7229b99a8beceeb4f8c7e4146f68ce6470
helo99q <lenillaishram750@gmail.com>
2025-03-15 09:55:51 +0530

    simple digit

---
+++ sum_of_digit/demo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class demo {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = scanner.nextInt();
+        int sum = 0;
+        while(number!= 0){
+            int demo = number % 10;
+            sum += demo;
+            number /=10;
+        }
+        System.out.println("The sum of the digits is: " +sum);
+    }
+
+    
+}
